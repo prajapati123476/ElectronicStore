7675145a1b818ae6a5aabbe7b29d7fcda206bb00
Vinay Prajapati <prajapativishwa671@gmail.com>
2023-08-19 04:15:25 +0530

    order controller

---
+++ src/main/java/com/lcwd/electronics/store/controllers/OrderController.java
@@ -0,0 +1,58 @@
+package com.lcwd.electronics.store.controllers;
+
+import com.lcwd.electronics.store.dtos.ApiResponseMessage;
+import com.lcwd.electronics.store.dtos.CreateOrderRequest;
+import com.lcwd.electronics.store.dtos.OrderDto;
+import com.lcwd.electronics.store.dtos.PageableResponse;
+import com.lcwd.electronics.store.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    //crate
+    @PostMapping
+    public ResponseEntity<OrderDto> createOrder(@RequestBody CreateOrderRequest request) {
+        OrderDto order = orderService.createOrder(request);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<ApiResponseMessage> removeOrder(@PathVariable String orderId) {
+        orderService.removeOrder(orderId);
+        ApiResponseMessage responseMessage = ApiResponseMessage.builder()
+                .success(true)
+                .status(HttpStatus.OK)
+                .message("deleted ...")
+                .build();
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+
+    }
+
+    //get orders of the user
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<OrderDto>> getOrderOfUser(@PathVariable String userId) {
+        List<OrderDto> orderOfUser = orderService.getOrderOfUser(userId);
+        return new ResponseEntity<>(orderOfUser, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageableResponse<OrderDto>> getOrders(
+            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "title", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+        PageableResponse<OrderDto> orders = orderService.getOrders(pageNumber, pageSize, sortBy, sortDir);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+}
